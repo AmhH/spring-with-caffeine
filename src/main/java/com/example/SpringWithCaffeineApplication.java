dca634f212b33ec84b07fa973451455590e9e2dd
Amh <abaams78@gmail.com>
2021-04-20 21:43:13 -0500

    caffeine cache with spring boot

--- src/main/java/com/example/SpringWithCaffeineApplication.java
+++ src/main/java/com/example/SpringWithCaffeineApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringWithCaffeineApplication {
 
 	public static void main(String[] args) {
+		//new SpringApplicationBuilder(SpringWithCaffeineApplication.class).run(args);
 		SpringApplication.run(SpringWithCaffeineApplication.class, args);
 	}
 
